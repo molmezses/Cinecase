1374d2aa25addae04690506ec50876050d5ef946
molmezses <mustafaolmezses@gmail.com>
2026-03-02 01:01:52 +0300

    chore: add iOS Xcode project

---
+++ ios/CineCase/CineCase/CineCaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineCaseApp.swift
+//  CineCase
+//
+//  Created by mustafaolmezses on 2.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct CineCaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
